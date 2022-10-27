5ebaf919865cd885b93002e2868434d8ef297879
Alexander <algle11@student.sdu.dk>
2022-10-27 11:44:39 +0200

    added extra class

---
+++ Server_OPCUA/src/main/java/org/eclipse/milo/examples/server/folderCreator.java
@@ -0,0 +1,9 @@
+package org.eclipse.milo.examples.server;
+
+import org.eclipse.milo.opcua.sdk.server.OpcUaServer;
+
+public class folderCreator extends Namespace{
+    folderCreator(OpcUaServer server) {
+        super(server);
+    }
+}
